e31f26c68241a684436092a764883fb1f72a2b4c
Andre B <31899523+drebass@users.noreply.github.com>
2018-01-26 16:23:53 -0700

    Update MeasurementUnitsConverter.c

--- MeasurementUnitsConverter.c
+++ MeasurementUnitsConverter.c
@@ -5,7 +5,8 @@ typedef struct {
   int i;
   char name[32];
 }unit;
-  
+
+
 void caclTotalSun (double *totalSum, double *partInLiters, double *partInGallons, double *sumLiters, double *sumGallons ) {
   int i;
   for( i=65;i<69;i++){
@@ -22,31 +23,37 @@ void caclTotalSun (double *totalSum, double *partInLiters, double *partInGallons
  *sumLiters *= 3.875;
  *totalSum = *sumGallons + *sumLiters;
 }
-}
-}
- void printConverted (unit conv[7], double * totalSum){
+
+
+void printConverted (unit conv[7], double * totalSum){
+  int i;
   printf("Here are the converted values:\n");
- for ( i = 0; i < 7; i++ ) {
- printf("%.0f %s \n", conv[i].value * (*totalSum), conv[i].name);
- }
-  void exitOrNot(char *exitChoice) {
- printf("\nExit product mix calculation (Y to exit)? \n");
- scanf("%s", exitChoice);
-  int main() {
- unit conv[7] = {{1, " US gallons"}, {8, " US quarts"}, {8, " US pints"}, {16, "
- US cups"}, {128, " US fluid ounces"}, {256, " US tablespoons"}, {768, "US teaspoons"}};
- int i, j;
- double partInLiters, partInGallons;
- double sumLiters = 0;
- double sumGallons = 0;
- double totalSum = 0;
- char exitChoice;
- printf("Welcome to the measurements unit convertor\n\n");
- do {
- caclTotalSun(&totalSum, &partInLiters, &partInGallons, &sumLiters, &sumGallons);
- printConverted(conv, &totalSum);
- exitOrNot(&exitChoice);
- } while (exitChoice != 'Y');
- system("pause");
- return 0;
- }
+  for ( i = 0; i < 7; i++ ) {
+    printf("%.0f %s \n", conv[i].value * (*totalSum), conv[i].name);
+  }
+}
+
+
+void exitOrNot(char *exitChoice) {
+  printf("\nExit product mix calculation (Y to exit)? \n");
+  scanf("%s", exitChoice);
+}
+
+
+int main() {
+  unit conv[7] = {{1, " US gallons"}, {8, " US quarts"}, {8, " US pints"}, {16, "US cups"}, {128, " US fluid ounces"}, {256, " US tablespoons"}, {768, "US teaspoons"}};
+  int i, j;
+  double partInLiters, partInGallons;
+  double sumLiters = 0;
+  double sumGallons = 0;
+  double totalSum = 0;
+  char exitChoice;
+  printf("Welcome to the measurements unit convertor\n\n");
+  do {
+    caclTotalSun(&totalSum, &partInLiters, &partInGallons, &sumLiters, &sumGallons);
+    printConverted(conv, &totalSum);
+    exitOrNot(&exitChoice);
+  }while (exitChoice != 'Y');
+    system("pause");
+    return 0;
+}
